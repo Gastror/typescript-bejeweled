c63502ea847a2886365d296d1d1c3a9e5248ff92
Berry van Mondfrans <personal@berryvm.com>
2018-03-08 17:11:11 +0100

    Added colour configuration

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,4 +1,4 @@
-import { px, url } from 'csx';
+import { px, url, color } from 'csx';
 
 export namespace BaseConstants {
   export const containerSelector =  '#approot';
@@ -6,6 +6,21 @@ export namespace BaseConstants {
 
 export namespace StyleConstants {
   export const appSize = px(700);
+
+  export namespace Colours {
+    export const BackgroundPrimary = color('#282a36');
+    export const BackgroundSecondary = color('#44475a');
+    export const BackgroundTertiary = color('#666666');
+    export const Foreground	= color('#f8f8f2');
+    export const Cyan	= color('#8be9fd');
+    export const Green	= color('#50fa7b');
+    export const Orange = color('#ffb86c');
+    export const Pink = color('#ff79c6');
+    export const Purple = color('#bd93f9');
+    export const Red = color('#ff5555');
+    export const Yellow = color('#f1fa8c');
+    export const Sandstone = color('#cfcfc2');
+  }
 
   export namespace Font {
     export const source = url('https://fonts.googleapis.com/css?family=Syncopate');
--- src/pages/Base/style.ts
+++ src/pages/Base/style.ts
@@ -1,10 +1,16 @@
 import { style } from 'typestyle';
-import { viewHeight, viewWidth, rgb } from 'csx/lib';
+import { viewHeight, viewWidth } from 'csx/lib';
 
 import { StyleConstants } from '../../core';
 import { gridAreas } from '../../utility/formatters';
 
-const { appSize } = StyleConstants;
+const {
+  appSize,
+  Colours: {
+    BackgroundPrimary,
+    Foreground
+  }
+} = StyleConstants;
 
 export const layout = style({
   display: 'grid',
@@ -12,10 +18,11 @@ export const layout = style({
   gridTemplateRows: `1fr ${appSize} 1fr`,
   gridTemplateAreas: gridAreas(['. . .', '. content .', '. . .']),
   height: viewHeight(100),
-  width: viewWidth(100)
+  width: viewWidth(100),
+  backgroundColor: BackgroundPrimary.toString(),
+  color: Foreground.toString()
 });
 
 export const mainSection = style({
   gridArea: 'content',
-  background: rgb(255, 0, 0).toString()
 });
